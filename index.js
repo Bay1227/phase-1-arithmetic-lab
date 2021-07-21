54f50ea1c7f5568f15089a25a1dd48440a04f3fb
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-07-21 13:41:17 -0700

    Completed Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+let num3 = 16
+let num4 = 4
 
-// Write your code here
+const multiply = num1 * num2
+
+let random = 0;
+random++;
+
+let mod = 4;
+const divide = num3 / num4
+
+
+const max = Math.max(1, 2, 3, 20);
